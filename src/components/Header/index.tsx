8941970fa3b20b5a46b2c49a54b2d565ad588d3f
Matheus Emídio <emidiodeveloper@gmail.com>
2022-01-01 17:41:48 -0300

    create context

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,17 @@
-import { useState } from 'react';
-import Modal from 'react-modal'
 import logoImg from '../../assets/logo.svg'
 
 import { Container, Content } from './styles'
 
-export function Header() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+interface headerProps {
+  onOpenNewTransactionModal: () => void;
+}
 
-  function handleOpenNewTransactionModal() {
-    setIsNewTransactionModalOpen(true)
-  }
-
-  function handleCloseNewTransactionModal() {
-    setIsNewTransactionModalOpen(false)
-  }
-  
+export function Header({ onOpenNewTransactionModal }: headerProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button" onClick={handleOpenNewTransactionModal}>
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
       </Content>
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Vendas',
+          amount: 1500000,
+          createdAt: new Date('2022-02-20 09:00:00')
+
+        },
+        {
+          id: 2,
+          title: 'Dízimo',
+          type: 'withdraw',
+          category: 'Dízimo',
+          amount: 150000,
+          createdAt: new Date('2022-02-21 10:00:00')
+
+        }
+      ],
+    })
+  },
+  
   routes () {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
     })
   }
 })
